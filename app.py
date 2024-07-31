6d15be383354175092d5272a1d1eac48aeaf4d0e
chunstartup <85015985+chunstartup@users.noreply.github.com>
2024-07-31 17:32:49 +0800

    Test 3

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def scrape_squad():
     url = "https://www.transfermarkt.com/fc-arsenal/startseite/verein/11"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    names_arr = []
-    for name in soup.find_all("a", class_="hauptlink"):
-        names_arr.append(name.decode_contents())
-    return names_arr
+    name_arr = []
+    for name in soup.find_all("ul", class_="data-header__items"):
+        name_arr.append(name.text)
+    return name_arr
