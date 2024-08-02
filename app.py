9fa1d78250fbf77894dcfae66df9374cee0681cf
chunstartup <85015985+chunstartup@users.noreply.github.com>
2024-08-02 16:43:17 +0800

    Test

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
 import requests
 from bs4 import BeautifulSoup
+import json
 app = Flask(__name__)
 
 def fetch(url):
-    print("fetch function")
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
@@ -23,15 +23,14 @@ def hello_world():
 
 @app.route('/input_page', methods=['GET', 'POST'])
 def input_page():
-    html = "<form method='post' action='/input_page'><input type='text' name='url' />" \
+    html = "<form method='post' action='/input_page'><input type='text' name='url' /><input type='text' name='css_selector' />" \
             "</br>" \
            "<button type='submit'>Submit</button></form>"
     print(f"Html: {html}")
            
     if request.method == 'POST': 
-        print(f"url: {request.values['url']}")
         result = fetch(request.values['url'])
-        return html + '<textarea id="w3review" name="w3review" rows="4" cols="50">' + ''.join(result) + '</textarea>'
+        return html + '<pre><code>' + json.dumps(result) + '</code></pre>'
 
     return html
 
@@ -60,7 +59,7 @@ def scrape_squad2():
     
     soup = BeautifulSoup(response.content, "html.parser")
     name_arr = []
-    tbody = soup.select_one('div.box div#yw1 table.items > tbody')
-    for name in tbody.select('div.box div#yw1 table.items > tbody > tr > td.posrela > table.inline-table td.hauptlink > a'):
+    # tbody = soup.select_one('div.box div#yw1 table.items > tbody')
+    for name in soup.select('div.box div#yw1 table.items > tbody > tr > td.posrela > table.inline-table td.hauptlink > a'):
         name_arr.append(" ".join(name.text.split()))
     return name_arr
