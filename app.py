8d8d822a8842f9e4f6b2bb7c5584e6393e2023a6
chunstartup <85015985+chunstartup@users.noreply.github.com>
2024-08-02 15:31:32 +0800

    Test with form

--- app.py
+++ app.py
@@ -3,9 +3,36 @@ import requests
 from bs4 import BeautifulSoup
 app = Flask(__name__)
 
+def fetch(url):
+    print("fetch function")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    name_arr = []
+    for name in soup.find_all("ul", class_="data-header__items"):
+        name_arr.append(name.text)
+    return name_arr
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/input_page', methods=['GET', 'POST'])
+def input_page():
+    html = "<form method='post' action='/input_page'><input type='text' name='url' />" \
+            "</br>" \
+           "<button type='submit'>Submit</button></form>"
+    print(f"Html: {html}")
+           
+    if request.method == 'POST': 
+        print(f"url: {request.values['url']}")
+        result = fetch(request.values['url'])
+        return html + '<textarea id="w3review" name="w3review" rows="4" cols="50">' + result + '</textarea>'
+
+    return html
 
 @app.route('/scrape_squad')
 def scrape_squad():
@@ -23,6 +50,7 @@ def scrape_squad():
     
     soup = BeautifulSoup(response.content, "html.parser")
     name_arr = []
-    for name in soup.find_all("ul", class_="data-header__items"):
+    tbody = soup.select_one('div.box div#yw1 table.items > tbody')
+    for name in tbody.select('tr > td.posrela > table.inline-table td.hauptlink > a'):
         name_arr.append(name.text)
     return name_arr
