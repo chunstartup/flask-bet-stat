5f0c7b290403a56e5c084e9a66d4470e5abf52cb
chunstartup <85015985+chunstartup@users.noreply.github.com>
2024-08-02 16:51:38 +0800

    With css_selector input & prettier json

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import json
 app = Flask(__name__)
 
-def fetch(url):
+def fetch(url, css_selector):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
@@ -12,8 +12,7 @@ def fetch(url):
 
     soup = BeautifulSoup(response.content, "html.parser")
     name_arr = []
-    tbody = soup.select_one('div.box div#yw1 table.items > tbody')
-    for name in tbody.select('tr > td.posrela > table.inline-table td.hauptlink > a'):
+    for name in soup.select(css_selector):
         name_arr.append(" ".join(name.text.split()))
     return name_arr
 
@@ -23,34 +22,20 @@ def hello_world():
 
 @app.route('/input_page', methods=['GET', 'POST'])
 def input_page():
-    html = "<form method='post' action='/input_page'><input type='text' name='url' /><input type='text' name='css_selector' />" \
+    html = "<form method='post' action='/input_page'><input type='text' name='url' value='https://www.transfermarkt.com/fc-arsenal/startseite/verein/11' />" \
+            "</br>" \
+            "<input type='text' name='css_selector' value='div.box div#yw1 table.items > tbody > tr > td.posrela > table.inline-table td.hauptlink > a' />" \
             "</br>" \
            "<button type='submit'>Submit</button></form>"
-    print(f"Html: {html}")
            
     if request.method == 'POST': 
-        result = fetch(request.values['url'])
-        return html + '<pre><code>' + json.dumps(result) + '</code></pre>'
+        result = fetch(request.values['url'], request.values['css_selector'])
+        return html + '<pre><code>' + json.dumps(result, indent=4) + '</code></pre>'
 
     return html
 
 @app.route('/scrape_squad')
 def scrape_squad():
-    url = "https://www.transfermarkt.com/fc-arsenal/startseite/verein/11"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
-    response = requests.get(url, headers=headers)
-    
-    soup = BeautifulSoup(response.content, "html.parser")
-    name_arr = []
-    tbody = soup.select_one('div.box div#yw1 table.items > tbody')
-    for name in tbody.select('tr > td.posrela > table.inline-table td.hauptlink > a'):
-        name_arr.append(" ".join(name.text.split()))
-    return name_arr
-
-@app.route('/scrape_squad2')
-def scrape_squad2():
     url = "https://www.transfermarkt.com/fc-arsenal/startseite/verein/11"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
