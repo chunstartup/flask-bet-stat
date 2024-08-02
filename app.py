825707e99f85cac392c4f2d2e93c6f79f3e42c3f
chunstartup <85015985+chunstartup@users.noreply.github.com>
2024-08-02 15:00:39 +0800

    Test

--- app.py
+++ app.py
@@ -9,8 +9,18 @@ def hello_world():
 
 @app.route('/scrape_squad')
 def scrape_squad():
+    print('hi')
+    
     url = "https://www.transfermarkt.com/fc-arsenal/startseite/verein/11"
-    response = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
+
+    # Debugging: Print response status code and text
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Text: {response.text[:100]}")  # Only print the first 100 characters
+    
     soup = BeautifulSoup(response.content, "html.parser")
     name_arr = []
     for name in soup.find_all("ul", class_="data-header__items"):
